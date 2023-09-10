bdb35bf63323514ed9e385d02da1fc442ea032fb
hesham944 <heshamashour9449@gmail.com>
2023-09-10 12:48:38 +0300

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unisted.h>
+/**
+ * main - Entry point
+ * Return:1
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stout);
+	return (1);
+
+}
