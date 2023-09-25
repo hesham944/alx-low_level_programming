5342aefdcbd88757d4a641b6a6d99b1d56d1dff6
hesham944 <heshamashour9449@gmail.com>
2023-09-25 22:05:12 +0300

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*mul - multiplies two integers.
+*@a: int number
+*@b: int number
+*Return: always 0
+**/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_line- prints a straight line
+*@n: number of times the character _ should be printed
+*Return: no return
+**/
+
+void print_line(int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
